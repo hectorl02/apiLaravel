1beeb2ca1361871bbd985fc37a94caad189c05a5
Hector Lopez <hectorlopezp02@gmail.com>
2022-04-08 00:05:25 -0500

    crea controlador v1 muestra 1 post por ruta api

--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,21 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
+
+Route::apiResource('v1/post', \App\Http\Controllers\Api\v1\PostController::class)->only('show');
+
+// add
+// Route::apiResource('v1/posts', PostV1::class)
+//     ->only(['index','show','destroy'])
+//     ->middleware('auth:sanctum');
+// Route::apiResource('v2/posts', PostV2::class)
+//     ->only(['index','show',])
+//     ->middleware('auth:sanctum');
+
+// Route::post('login',[
+//     LoginController::class,
+//     'login'
+// ]);
