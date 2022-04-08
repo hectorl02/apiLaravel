4ca442abf2962b40031e3e44d331366c3ebb4c87
Hector Lopez <hectorlopezp02@gmail.com>
2022-04-07 23:20:28 -0500

    se config ruta, controlador, vista,inst tailwind

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -3,8 +3,17 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+//add
+use App\Models\Post;
 
 class PostController extends Controller
 {
-    //
+    //add
+    public function index()
+    {
+        //retorna el index con esta información
+        return view('index',[
+            'posts'=>Post::latest()->paginate()
+        ]);
+    }
 }
--- app/Models/Post.php
+++ app/Models/Post.php
@@ -8,4 +8,20 @@ use Illuminate\Database\Eloquent\Model;
 class Post extends Model
 {
     use HasFactory;
+    // add
+    public function getExcerptAttribute()
+    {
+        return substr($this->content,0,120);
+    }
+
+    //cuando fue publicado
+    public function getPublishedAtAttribute()
+    {
+        return $this->created_at->format('d/m/Y');
+    }
+
+    public function user()
+    {
+        return $this->beLongsTo(User::class);
+    }
 }
